d74d4cc80a5e47a63367bb0d6aa23435257600ee
SoDKo0331 <your-email@example.com>
2025-09-24 13:34:33 +0000

    fa

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    trailingSlash: true,
-    images: {
-      unoptimized: true
-    },
-    // Development хувьд cross-origin request зөвшөөрөх
-    allowedDevOrigins: [
-      '3000-firebase-anujingit-1758685821668.cluster-44kx2eiocbhe2tyk3zoyo3ryuo.cloudworkstations.dev'
-    ],
-    // GitHub Pages deployment хувьд
-    basePath: process.env.NODE_ENV === 'production' ? '/Anujin' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/Anujin/' : '',
-  }
-  
-  export default nextConfig
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  // Development хувьд cross-origin request зөвшөөрөх
+  allowedDevOrigins: [
+    '3000-firebase-anujingit-1758685821668.cluster-44kx2eiocbhe2tyk3zoyo3ryuo.cloudworkstations.dev'
+  ],
+  // GitHub Pages deployment хувьд (repository нэрээ оруулна уу)
+  basePath: process.env.NODE_ENV === 'production' ? '/Anujin' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Anujin/' : '',
+}
+
+module.exports = nextConfig
